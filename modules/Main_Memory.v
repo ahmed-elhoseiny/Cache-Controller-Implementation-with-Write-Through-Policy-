c8b6594c358ff0141277268bbdf43379f72b5aee
Ahmed Mahmoud Hoseiny <ahmedelhosseini53@gmail.com>
2023-09-01 15:55:18 +0300

    Update Main_Memory.v

--- modules/Main_Memory.v
+++ modules/Main_Memory.v
@@ -3,17 +3,17 @@ module Main_Memory #(
               DEPTH = 1024
 ) (
     input wire                    clk,reset,        
-    input wire [$log2(DEPTH)-1:0] address;
+    input wire [$clog2(DEPTH)-1:0] address,
     input wire                    write_en,  
-//    input wire                    read_en,    
+    input wire                    read_en,    
     input wire [WIDTH-1:0]        write_data,
 
     output wire                   ready, 
-    output wire [WIDTH-1:0]       read_data 
+    output reg [WIDTH-1:0]       read_data 
 );
     
 reg		[WIDTH-1:0] 	RAM		[0:DEPTH-1] ;
-wire    [3-1:0]        count;
+reg    [3-1:0]        count;
 
 integer k ;
 always @(posedge clk or negedge reset) 
@@ -22,22 +22,22 @@ begin
     begin
         for ( k=0 ; k<=(DEPTH-1) ; k=k+1) 
         begin
-            RAM[k] <= WIDTH'b0;
+            RAM[k] <= 'b0;
         end
         count <= 3'b0;
-    end else if ((write_en || read_en)  && !(count == 3'b4;)) 
+    end else if ((write_en || read_en)  && !(count == 3'd4)) 
     begin
-        count <=  count + 1'b1;
-    end else if ((write_en ) && (count == 3'b4)) 
+        count <=  count + 1'd1;
+    end else if ((write_en ) && (count == 3'd4)) 
     begin
         RAM[address] <= write_data;
         count <= 3'b0;
-    end else if ((read_en ) && (count == 3'b4)) 
+    end else if ((read_en ) && (count == 3'd4)) 
     begin
         read_data <= RAM[address];
         count <= 3'b0;
     end
 end
 
-assign ready = ((count == 3'b4)) ? 1'b1 : 1'b0;
+assign ready = ((count == 3'd4)) ? 1'b1 : 1'b0;
 endmodule
