0643fe190a6b4d49ec196dd5880cad2657d17df7
Ahmed Mahmoud Hoseiny <ahmedelhosseini53@gmail.com>
2023-09-01 10:25:01 +0300

    Create Main_Memory.v

---
+++ modules/Main_Memory.v
@@ -0,0 +1,16 @@
+module Main_Memory #(
+    parameter WIDTH = 32,
+              DEPTH = 1024
+) (
+    input wire                    clk,reset,        
+    input wire [$log2(DEPTH)-1:0] address;
+    input wire                    write_en,  
+//    input wire                    read_en,    
+    input wire [WIDTH-1:0]        write_data, 4
+
+    output wire [WIDTH-1:0]       read_data 
+);
+    
+
+
+endmodule
